e1f2aede3e499ed81ea38e83972ce2d93f2d66f3
pekpuglia <pepuglia@gmail.com>
2020-09-05 17:34:37 -0300

    Energia ainda errada

--- Diatomic.js
+++ Diatomic.js
@@ -6,7 +6,7 @@ Depende da classe de átomo individual.
 function project(v, a) {
     //projeta v em a
     //<v, a>/||a||^2 * a
-    return p5.Vector.mult(a, v.dot(a)).div(a.magSq())
+    return p5.Vector.mult(a, v.dot(a) / (a.magSq()));
 }
 
 class Diatomic {
@@ -39,27 +39,45 @@ class Diatomic {
         return [pos_atom1, pos_atom2];
     }
     wall_collide(i, normal) {
+        //ERRADO!!! não conserva energia
+        //tentar com deltaL
         //i é índice do átomo que colidiu com a parede
         //collide com a parede
         //velocidade inicial do átomo que colidiu
-        let collided_atom_vel = p5.Vector.add(this.cm_vel, p5.Vector.cross(this.omega, this.d_CM[i]));
+        let collided_atom_vel = p5.Vector.add(this.cm_vel, p5.Vector.cross(this.omega, p5.Vector.mult(this.n, this.d_CM[i])));
         //velociade inicial do outro átomo
-        let other_atom_vel = p5.Vector.add(this.cm_vel, p5.Vector.cross(this.omega, this.d_CM[1 - i]));
+        let other_atom_vel = p5.Vector.add(this.cm_vel, p5.Vector.cross(this.omega, p5.Vector.mult(this.n, -this.d_CM[1 - i])));
+        let e = this.atoms[i].m * collided_atom_vel.magSq() / 2 + this.atoms[1 - i].m * other_atom_vel.magSq() / 2;
+        console.log(e);
         //reflete a velocidade do átomo que colidiu na direção normal
         collided_atom_vel.sub(project(collided_atom_vel, normal).mult(2));
-        //a projeção das velocidades dos centros dos átomos na linha que os liga
-        //deve ser igual (corpo rígido)
-        other_atom_vel.sub(project(other_atom_vel, this.n)).add(project(collided_atom_vel, this.n));
-
+        //faz colisão inelástica entre os átomos
+        other_atom_vel.sub(project(other_atom_vel, this.n)).add(project(collided_atom_vel, this.n).mult(this.atoms[i].m / (this.atoms[i].m + this.atoms[1 - i].m)));
+        collided_atom_vel.sub(project(collided_atom_vel, this.n)).add(project(other_atom_vel, this.n));
+        e = this.atoms[i].m * collided_atom_vel.magSq() / 2 + this.atoms[1 - i].m * other_atom_vel.magSq() / 2 - e;
+        console.log(e);
         //calcula novo omega e vCM
         this.cm_vel = p5.Vector.add(collided_atom_vel, other_atom_vel).div(2);
         //omega = r x (v - vCM)
-        this.omega = p5.Vector.cross(p5.Vector.mult(this.n, this.d_CM[i]), p5.Vector.sub(collided_atom_vel, this.cm_vel));
+        this.omega = p5.Vector.cross(p5.Vector.mult(this.n, 1 / this.d_CM[i]), p5.Vector.sub(collided_atom_vel, this.cm_vel));
     }
     update(dt) {
         this.translate(dt);
         this.rotate(dt);
         //checa colisão com as paredes
+        let poss = this.atom_centers();
+        let i;
+        let normal = null;
+        for (i = 0; i < 2; i++) {
+            //corrigir "ultrapassagem"
+            if (poss[i].x + this.atoms[i].radius > width) {
+                normal = createVector(-1, 0);
+                break;
+            }
+        }
+        if (normal != null) {
+            this.wall_collide(i, normal);
+        }
     }
     draw() {
         let centers = this.atom_centers();
