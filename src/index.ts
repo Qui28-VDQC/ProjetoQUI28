6de339999e7668edc5aa5d01db549f8e1ff00ee9
Vinícius Freitas de Almeida <vinifreitas.d.a@gmail.com>
2020-10-31 07:23:05 -0300

    Inserted render loop

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,30 @@
 //window.draw = draw;
 import { Drawing } from './Drawing'
 import { vec2 } from "gl-matrix"
+import { setup, draw, particles } from './Simulator'
 
 let p: vec2 = [1.0, 2.0];
 
-let draw = new Drawing();
+let ctx = new Drawing();
 
-draw.circle(p, 20.0);
+//draw.circle(p, 20.0);
+
+setup();
+
+console.log(particles);
+
+let start: number | undefined = undefined;
+
+function step(timestamp: number) {
+  if (start === undefined)
+    start = timestamp;
+  const elapsed = timestamp - start;
+
+  draw(ctx);
+
+  if (elapsed < 10000) { // Stop the animation after 10 seconds
+    window.requestAnimationFrame(step);
+  }
+}
+
+window.requestAnimationFrame(step);
