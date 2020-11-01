e5cbf2e53c72da74edae4cd23e63b9b12edfc269
Vinícius Freitas de Almeida <vinifreitas.d.a@gmail.com>
2020-11-01 01:28:08 -0300

    Removed instance of particles[], added update func

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@
 //THIS IS A HACK!!
 //window.setup = setup;
 //window.draw = draw;
-import { Drawing } from './Drawing'
-import { vec2 } from "gl-matrix"
-import { setup, draw, particles } from './Simulator'
+import { Drawing } from './Drawing';
+import { vec2 } from "gl-matrix";
+import { setup, draw, update_simulation } from './Simulator';
 
 let p: vec2 = [1.0, 2.0];
 
@@ -15,16 +15,20 @@ let ctx = new Drawing();
 
 setup();
 
-console.log(particles);
 
 let start: number | undefined = undefined;
+let prev_elapsed = 0.0;
 
 function step(timestamp: number) {
   if (start === undefined)
     start = timestamp;
   const elapsed = timestamp - start;
-
+  const dt = 0.001*(elapsed - prev_elapsed);
+  prev_elapsed = elapsed;
+  
+  ctx.clearCanvas();
   draw(ctx);
+  update_simulation(dt);
 
   if (elapsed < 10000) { // Stop the animation after 10 seconds
     window.requestAnimationFrame(step);
